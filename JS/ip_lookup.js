8a8d067fb99beff365d66690a031f0c68d26f25f
S-Kel <41603185+S-Kel@users.noreply.github.com>
2018-12-07 15:16:57 +1100

    Update ip_lookup.js

--- JS/ip_lookup.js
+++ JS/ip_lookup.js
@@ -1,8 +1,7 @@
 const ipLookUp = function(resolve) {
-    try{
     fetch('https://ip-api.com/json')
         .then((res) => res.json())
         .catch((error) => undefined)
         .then((data) => resolve(data))
-    }catch(error){return undefined;}
+    
 }
